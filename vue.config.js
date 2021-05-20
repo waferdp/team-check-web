3f72c69c0dac0a5ca384d4c61ca42779c99b7b24
Jacob Brännström <jacob.brannstrom@gmail.com>
2021-05-20 21:04:17 +0200

    Update title and favicon
    - Team selector is now MVP ready?

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     configureWebpack: {
       devtool: 'source-map'
+    },
+    chainWebpack: config => {
+      config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Team Self Assessment'
+        return args
+      })
     }
   }
